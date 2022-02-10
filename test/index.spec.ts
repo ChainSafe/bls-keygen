6a99a63f80c6b920a775cd1b8f499ae66599df2c
Cayman <caymannava@gmail.com>
2022-02-10 17:27:14 -0600

    chore: update dependencies
    BREAKING CHANGE: Replace `Buffer` with `Uint8Array` in public APIs

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -5,15 +5,15 @@ import {generateMnemonic} from "bip39";
 describe("random private key", function () {
 
   it("should generate with given entropy", function () {
-    const key = generateRandomSecretKey(Buffer.alloc(32)).toString("hex");
-    const key1 = generateRandomSecretKey(Buffer.alloc(32)).toString("hex");
-    expect(key).to.not.be.equal(key1);
+    const key = generateRandomSecretKey(Buffer.alloc(32));
+    const key1 = generateRandomSecretKey(Buffer.alloc(32));
+    expect(key).to.not.deep.equal(key1);
   });
 
   it("should generate without given entropy", function () {
-    const key = generateRandomSecretKey().toString("hex");
-    const key1 = generateRandomSecretKey().toString("hex");
-    expect(key).to.not.be.equal(key1);
+    const key = generateRandomSecretKey();
+    const key1 = generateRandomSecretKey();
+    expect(key).to.not.deep.equal(key1);
   });
 
 });
@@ -25,7 +25,7 @@ describe("private key from mnemonic", function () {
     const key = deriveKeyFromMnemonic(mnemonic);
     const key1 = deriveKeyFromMnemonic(mnemonic);
     expect(key).to.not.be.null;
-    expect(key.toString("hex")).to.be.equal(key1.toString("hex"));
+    expect(key).to.deep.equal(key1);
   });
 
   it("should generate using given path", function () {
@@ -33,7 +33,7 @@ describe("private key from mnemonic", function () {
     const key = deriveKeyFromMnemonic(mnemonic, "m/12381/3600/0/1");
     const key1 = deriveKeyFromMnemonic(mnemonic, "m/12381/3600/0/2");
     expect(key).to.not.be.null;
-    expect(key.toString("hex")).to.not.be.equal(key1.toString("hex"));
+    expect(key).to.not.deep.equal(key1);
   });
 
 });
@@ -45,7 +45,7 @@ describe("private key from entropy", function () {
     const key = deriveKeyFromEntropy(seed);
     const key1 = deriveKeyFromEntropy(seed);
     expect(key).to.not.be.null;
-    expect(key.toString("hex")).to.be.equal(key1.toString("hex"));
+    expect(key).to.deep.equal(key1);
   });
 
   it("should generate child key using given path", function () {
@@ -53,7 +53,7 @@ describe("private key from entropy", function () {
     const key = deriveKeyFromEntropy(seed, "m/12381/3600/0/1");
     const key1 = deriveKeyFromEntropy(seed, "m/12381/3600/0/2");
     expect(key).to.not.be.null;
-    expect(key.toString("hex")).to.not.be.equal(key1.toString("hex"));
+    expect(key).to.not.deep.equal(key1);
   });
 
 });
